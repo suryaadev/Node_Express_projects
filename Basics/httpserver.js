b24623a7655988e3639fc4194dc3c0b5105a7fdc
suryaadev <rsurya1760@gmail.com>
2023-02-15 21:07:48 +0530

    course completed

--- Basics/httpserver.js
+++ Basics/httpserver.js
@@ -8,6 +8,10 @@ const server = http.createServer((req, res) => {
   if (req.url == "/") {
     res.end("<h1>This is HTTP server response</h1>");
   }
+  else if (req.url == "/about") {
+    res.end("<h1>This is about page</h1>");
+    res.end("<p>{req}</p>");
+  }
 });
 
 server.listen(port, () => console.log(`Server is running on port ::: ${port}`));
