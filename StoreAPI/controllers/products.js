e4de654af305103afa92df527bf3906b7ccdadf8
suryaadev <rsurya1760@gmail.com>
2023-04-04 19:16:40 +0530

    code completed

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -12,7 +12,61 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   try {
-    console.log(`weugn`);
+    const { featured, company, name, sort } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+      queryObject.featured = featured === "true" ? true : false;
+    }
+    if (company) {
+      queryObject.company = company;
+    }
+    if (name) {
+      queryObject.name = { $regex: name, $options: "i" };
+    }
+    if (numericFilters) {
+      const operatorMap = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte",
+      };
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericFilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      );
+      const options = ["price", "rating"];
+      filters = filters.split(",").forEach((item) => {
+        const [field, operator, value] = item.split("-");
+        if (options.includes(field)) {
+          queryObject[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+    let result = Product.find(queryObject);
+    // sort
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createdAt");
+    }
+
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result = result.select(fieldsList);
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await product.find(queryObject);
+    console.log(req.query);
+    res.status(200).json({ products, nbHits: products.length });
   } catch (error) {
     console.log(error);
   }
